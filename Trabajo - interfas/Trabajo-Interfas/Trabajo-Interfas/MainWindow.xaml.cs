dc401dfb8672721981a6e223764349ac4106c233
Taba98674 <133249277+Taba98674@users.noreply.github.com>
2023-09-05 21:25:34 -0500

    juancarlos

---
+++ Trabajo - interfas/Trabajo-Interfas/Trabajo-Interfas/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Trabajo_Interfas
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtener los valores de los cuadros de texto
+            string actividad = ActividadTextBox.Text;
+            string valorStr = ValorTextBox.Text;
+
+            // Validar la entrada (puedes agregar más validaciones según sea necesario)
+            if (string.IsNullOrEmpty(actividad) || string.IsNullOrEmpty(valorStr))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!double.TryParse(valorStr, out double valor))
+            {
+                MessageBox.Show("El valor ingresado no es válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Crear un objeto de gasto
+            Gasto nuevoGasto = new Gasto(actividad, valor);
+
+            // Agregar el nuevo gasto a la lista y actualizar el ListBox
+            ListaDeGastos.Add(nuevoGasto);
+            ActualizarListBox();
+
+            // Limpiar los cuadros de texto
+            ActividadTextBox.Clear();
+            ValorTextBox.Clear();
+        }
+
+        private void ActualizarListBox()
+        {
+            ListaDeGastosListBox.Items.Clear();
+            foreach (var gasto in ListaDeGastos)
+            {
+                ListaDeGastosListBox.Items.Add($"{gasto.Actividad}: ${gasto.Valor}");
+            }
+        }
+        private List<Gasto> ListaDeGastos = new List<Gasto>();
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Gasto
+    {
+        public string Actividad { get; set; }
+        public double Valor { get; set; }
+
+        public Gasto(string actividad, double valor)
+        {
+            Actividad = actividad;
+            Valor = valor;
+        }
+    }
+}
---
+++ Trabajo - interfas/convertidor/convertidor/convertidor/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace convertidor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Obtener la opción seleccionada del ComboBox
+            string opcionSeleccionada = ((ComboBoxItem)opcion.SelectedItem)?.Content.ToString();
+
+            switch (opcionSeleccionada)
+            {
+                case "Longitud":
+                    unid1.Text = "Metros:";
+                    unid2.Text = "Pies:";
+                    break;
+                case "Peso":
+                    unid1.Text = "Kilogramos:";
+                    unid2.Text = "Libras:";
+                    break;
+                case "Temperatura":
+                    unid1.Text = "Celsius:";
+                    unid2.Text = "Fahrenheit:";
+                    break;
+                default:
+                    unid1.Text = string.Empty;
+                    unid2.Text = string.Empty;
+                    break;
+            }
+        }
+
+        private void Convertir_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtener la opción seleccionada del ComboBox
+            string opcionSeleccionada = ((ComboBoxItem)opcion.SelectedItem)?.Content.ToString();
+
+            double valor;
+            if (!double.TryParse(unidad1.Text, out valor))
+            {
+                MessageBox.Show("Ingrese un número válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            double resultado = 0;
+
+            switch (opcionSeleccionada)
+            {
+                case "Longitud":
+                    resultado = ConvertirLongitud(valor);
+                    break;
+                case "Peso":
+                    resultado = ConvertirPeso(valor);
+                    break;
+                case "Temperatura":
+                    resultado = ConvertirTemperatura(valor);
+                    break;
+                default:
+                    MessageBox.Show("Seleccione una opción válida de conversión.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
+
+            unidad2.Text = resultado.ToString("F2");
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private double ConvertirLongitud(double metros)
+        {
+            return metros * 3.281; // Convertir metros a pies
+        }
+
+        private double ConvertirPeso(double kilogramos)
+        {
+            return kilogramos * 2.205; // Convertir kilogramos a libras
+        }
+
+        private double ConvertirTemperatura(double celsius)
+        {
+            return (celsius * 9 / 5) + 32; // Convertir Celsius a Fahrenheit
+        }
+    }
+}
